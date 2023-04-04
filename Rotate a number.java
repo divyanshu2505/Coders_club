e865bd990b5161e02afb4a4e2b3e0ba8efc398dd
Divyanshu <dm1537737@gmail.com>
2023-04-04 20:03:11 +0530

    rotate a number

---
+++ Rotate a number.java
@@ -0,0 +1,27 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        // System.out.println("Hello, World!");
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int k = scn.nextInt();
+        
+        int d=0;
+        int temp = n;
+        while(temp != 0){
+            d++;
+            temp = temp / 10;
+        }
+        k = k % d;
+        int pow = (int)(Math.pow(10,k));
+        int np = (int)(Math.pow(10,d-k));
+        
+        int a = n % pow;
+        int b = n / pow;
+        int ans = a * np + b;
+        System.out.println(ans);
+        
+    }
+}
