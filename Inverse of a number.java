724875b8884cd2f369740d18b93e340992d5159e
Divyanshu <dm1537737@gmail.com>
2023-04-05 19:33:34 +0530

    inverse

---
+++ Inverse of a number.java
@@ -0,0 +1,21 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        // System.out.println("Hello, World!");
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        int fv = 0;
+        int ans = 0;
+        while(n != 0){
+            fv++;
+            int d = n % 10;
+            int pow = (int)(Math.pow(10,d-1));
+            n = n / 10;
+            ans = ans + fv * pow;
+        }
+        System.out.println(ans);
+    }
+}
